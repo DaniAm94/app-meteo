428dbb741551c5082b9b674e1412e1ed93eff1a3
DaniAm94 <dani.amico94@gmail.com>
2024-10-29 13:23:42 +0100

    added a basic routing by using react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./layout/DefaultLayout";
+import HomePage from "./pages/homePage/HomePage";
+import FavouritesPage from "./pages/favouritesPage/FavouritesPage";
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
+      <Routes>
 
-    </>
+        {/* Lista rotte */}
+        <Route element={<DefaultLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="preferiti" element={<FavouritesPage />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
