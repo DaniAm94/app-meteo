d617369fa3c11395cb97e55d984e188ef7ef0b03
DaniAm94 <dani.amico94@gmail.com>
2024-10-31 14:42:10 +0100

    destructured weather conditions received as prop into the weather display component

--- src/components/weatherDisplay/WeatherDisplay.jsx
+++ src/components/weatherDisplay/WeatherDisplay.jsx
@@ -1,4 +1,10 @@
 const WeatherDisplay = ({ weatherConditions }) => {
+
+    // Se arrivano delle weather conditions, destrutturo l'oggetto (per comodità)
+    if (weatherConditions !== null) {
+        const { location, country, temperature_2m: temperature, apparent_temperature: apparentTemperature, weather_code: weatherCode, relative_humidity_2m: humidity, precipitation, cloud_cover: cloudCover, wind_speed_10m: windSpeed, wind_direction_10m: windDirection } = weatherConditions;
+    }
+
     return (
         weatherConditions === null ?
             '' :
