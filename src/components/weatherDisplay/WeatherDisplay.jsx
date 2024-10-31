2b1409060d75feb9214730ca1aaa78d0bb8cb2b9
DaniAm94 <dani.amico94@gmail.com>
2024-10-31 16:18:04 +0100

    handled weatherDisplay compoent rendering

--- src/components/weatherDisplay/WeatherDisplay.jsx
+++ src/components/weatherDisplay/WeatherDisplay.jsx
@@ -1,16 +1,27 @@
 const WeatherDisplay = ({ weatherConditions }) => {
 
-    // Se arrivano delle weather conditions, destrutturo l'oggetto (per comodità)
-    if (weatherConditions !== null) {
-        const { location, country, temperature_2m: temperature, apparent_temperature: apparentTemperature, weather_code: weatherCode, relative_humidity_2m: humidity, precipitation, cloud_cover: cloudCover, wind_speed_10m: windSpeed, wind_direction_10m: windDirection } = weatherConditions;
-    }
+    // Se le condizioni meteorologiche sono null blocco il rendering del componente
+    if (weatherConditions === null) return;
+
+    // Destrutturo weatherConditions e rinomino eventuali attributi
+    const {
+        place,
+        country,
+        temperature_2m: temperature,
+        apparent_temperature: apparentTemperature,
+        weather_code: weatherCode,
+        relative_humidity_2m: humidity,
+        precipitation,
+        cloud_cover: cloudCover,
+        wind_speed_10m: windSpeed,
+        wind_direction_10m: windDirection
+    } = weatherConditions;
+
 
     return (
-        weatherConditions === null ?
-            '' :
-            <section>
-                <h2>{weatherConditions.location}</h2>
-            </section>
+        <section>
+            <h2>{place}</h2>
+        </section>
     )
 }
 export default WeatherDisplay;
