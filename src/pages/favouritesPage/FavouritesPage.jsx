f10fea6e82bfbe9f292421df23f2e43fbc78d5c3
DaniAm94 <dani.amico94@gmail.com>
2024-11-01 19:23:27 +0100

    added function to fetch a location's weather conditions

--- src/pages/favouritesPage/FavouritesPage.jsx
+++ src/pages/favouritesPage/FavouritesPage.jsx
@@ -5,6 +5,41 @@ const FavouritesPage = () => {
 
     const [favourites, setFavourites, includesFavourite] = useFavourites()
 
+
+
+
+
+
+    /**
+     * Funzione che raccoglie le condizione metereologiche di una località passata come parametro
+     * @param {Object} location 
+     * @returns restituisce un oggetto con le condizioni meteorologiche
+     */
+    const fetchWeatherConditions = async (location) => {
+
+        // Configuro i miei params per la query string
+        const params = {
+
+            // Coordinate della località cercata
+            latitude: location.latitude,
+            longitude: location.longitude,
+
+            // Dati relativi al meteo attuale
+            current: "temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,cloud_cover,wind_speed_10m,wind_direction_10m",
+
+            timezone: "auto",
+
+        }
+
+        try {
+            const { data } = await axios.get(weatherUrl, { params });
+            console.log(data.current);
+            return data.current;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     return (
         <>
             <h1>Pagina preferiti</h1>
