d1f046972cedce4e3200ac4e616a0e1bf961298e
DaniAm94 <dani.amico94@gmail.com>
2024-11-01 17:19:55 +0100

    created a state to keep LocalStorage synchronized

--- src/hooks/useFavourites.jsx
+++ src/hooks/useFavourites.jsx
@@ -9,6 +9,24 @@ const useFavourites = () => {
     if (favourites === null) {
         localStorage.setItem("favourites", JSON.stringify([]))
     }
+
+    // Passo una funzione allo useState per evitare calcoli non necessari ogni volta che il componente viene  renderizzato
+    // Dato che la funzione verra invocata solo alla prima renderizzazione
+    const [state, setState] = useState(() => {
+
+        // Controllo se ci sono già dei preferiti nel localStorage altrimenti inizializzo un array vuoto 
+        const favourites = localStorage.getItem("favourites");
+        if (favourites === null) {
+            localStorage.setItem("favourites", JSON.stringify([]));
+        }
+
+        // Controllo il valore preso dal localStorage:
+        // Se è null assegno allo state un array vuoto
+        return favourites === null ?
+            [] :
+            // Altrimenti restituisco restituisco l'array di preferiti
+            JSON.parse(favourites);
+    });
     return (
         <>
 
