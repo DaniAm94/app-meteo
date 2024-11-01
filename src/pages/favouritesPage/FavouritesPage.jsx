479840c8e6f45c8498c060121432bfddb7442e2c
DaniAm94 <dani.amico94@gmail.com>
2024-11-01 19:21:15 +0100

    imported favourites' list into favourite page

--- src/pages/favouritesPage/FavouritesPage.jsx
+++ src/pages/favouritesPage/FavouritesPage.jsx
@@ -1,7 +1,22 @@
+import useFavourites from "../../hooks/useFavourites";
+
+
 const FavouritesPage = () => {
+
+    const [favourites, setFavourites, includesFavourite] = useFavourites()
+
     return (
         <>
             <h1>Pagina preferiti</h1>
+            <ul>
+                {
+                    favourites.map(favourite => <li
+                        key={favourite.id}
+                    >
+                        {favourite.name}
+                    </li>)
+                }
+            </ul>
         </>
     )
 }
