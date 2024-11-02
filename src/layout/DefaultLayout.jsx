e9177d7486ef091638f7ba7bccf84c4329585d2d
DaniAm94 <dani.amico94@gmail.com>
2024-11-02 14:57:18 +0100

    updated default layout style

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -16,14 +16,25 @@ const DefaultLayout = () => {
                 </nav>
             </header>
             <main >
-                <div className="container">
+                <div className="container h-100 d-flex align-items-center justify-content-center">
 
                     <Outlet></Outlet>
                 </div>
             </main>
             <footer >
-                <div className="container footer-content">
-                    Footer
+                <div className="container h-100">
+                    <div className="row row-cols-3 h-100 d-flex align-items-center">
+
+                        <p className="col d-flex flex-column flex-md-row gap-sm-1">© 2024 WeatherApp <span className="d-none d-md-inline">|</span>   <a href="https://www.linkedin.com/in/danilo-amico-6a6708303/?trk=opento_sprofile_topcard" target="_blank">Danilo Amico</a></p>
+                        <p className="col d-flex flex-column flex-md-row column-gap-md-1">Dati meteo forniti da <a href="https://open-meteo.com" target="_blank" rel="noopener noreferrer">Open Meteo API</a></p>
+                        <div className="footer-links col d-flex flex-column flex-md-row column-gap-md-1">
+                            <a href="mailto:dani.amico94@gmail.com">Contattami</a>
+                            <span className="d-none d-md-inline">
+                                |
+                            </span>
+                            <a href="https://github.com/DaniAm94/app-meteo" target="_blank" rel="noopener noreferrer">GitHub</a>
+                        </div>
+                    </div>
                 </div>
             </footer>
         </>
