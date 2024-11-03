3ec0b443c371e0d635d1567553b122ca4284768d
DaniAm94 <dani.amico94@gmail.com>
2024-11-03 15:37:43 +0100

    fixed app name into the footer

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -26,7 +26,7 @@ const DefaultLayout = () => {
                 <div className="container h-100">
                     <div className="row row-cols-3 h-100 d-flex align-items-center">
 
-                        <p className="col d-flex flex-column flex-md-row gap-sm-1">© 2024 WeatherApp <span className="d-none d-md-inline">|</span>   <a href="https://www.linkedin.com/in/danilo-amico-6a6708303/?trk=opento_sprofile_topcard" target="_blank">Danilo Amico</a></p>
+                        <p className="col d-flex flex-column flex-md-row gap-sm-1">© 2024 App Meteo <span className="d-none d-md-inline">|</span>   <a href="https://www.linkedin.com/in/danilo-amico-6a6708303/?trk=opento_sprofile_topcard" target="_blank">Danilo Amico</a></p>
                         <p className="col d-flex flex-column flex-md-row column-gap-md-1">Dati meteo forniti da <a href="https://open-meteo.com" target="_blank" rel="noopener noreferrer">Open Meteo API</a></p>
                         <div className="footer-links col d-flex flex-column flex-md-row column-gap-md-1">
                             <a href="mailto:dani.amico94@gmail.com">Contattami</a>
