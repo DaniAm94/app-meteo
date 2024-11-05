765f3d0ed86ef3d060426068297703ab65b49e44
DaniAm94 <dani.amico94@gmail.com>
2024-11-05 20:28:36 +0100

    completed temperature' trend chart

--- src/components/weatherDisplay/components/temperatureChart/TemperatureChart.jsx
+++ src/components/weatherDisplay/components/temperatureChart/TemperatureChart.jsx
@@ -1,34 +1,124 @@
-import { AreaChart, Area, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import { AreaChart, Area, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer, ReferenceLine } from 'recharts';
+import { useEffect, useState } from 'react';
+const baseUrl = import.meta.env.VITE_BASE_METEO_URL;
+import { format } from "date-fns"
+import axios from 'axios';
 
-const TemperatureChart = () => {
+const TemperatureChart = ({ location }) => {
 
-    const hourlyTemp = [
-        { time: '00:00', temperature: 12 },
-        { time: '01:00', temperature: 11 },
-        { time: '02:00', temperature: 10 },
-        { time: '03:00', temperature: 9 },
-        { time: '04:00', temperature: 8 },
-        { time: '05:00', temperature: 7 },
-        { time: '06:00', temperature: 6 },
-        { time: '07:00', temperature: 10 }
-    ];
 
-    const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 }];
+    const [tempTrend, setTempTrend] = useState(null)
+
+    const fetchWeatherData = async () => {
+
+        // Configurazione dei parametri per la richiesta API
+        const params = {
+            "latitude": location.latitude,
+            "longitude": location.longitude,
+            "hourly": "temperature_2m",
+            "timezone": "auto",
+            "forecast_days": 2
+        }
+
+        try {
+            const { data: res } = await axios.get(baseUrl, { params });
+            console.log(res)
+
+
+
+
+            setTempTrend(res.hourly.time.map((time, index) => ({
+                time,
+                temperature: res.hourly.temperature_2m[index]
+            })));
+        } catch (err) {
+            console.error("Errore durante il recupero dei dati meteo: ", err)
+        }
+    }
+
+
+
+    useEffect(() => {
+
+
+
+
+        fetchWeatherData();
+    }, [])
+
+
+
+
+    // Componente tooltip personalizzato
+    const CustomTooltip = ({ active, payload, label }) => {
+        if (active && payload && payload.length) {
+            const formattedDate = format(new Date(label), "HH:mm dd/MM/yyyy");
+            return (
+                <div style={{
+                    backgroundColor: '#f5f5f5',
+                    color: '#000',
+                    padding: '3px',
+                    border: '1px solid #ccc',
+                    borderRadius: '5px',
+                    fontSize: '10px' // Regola il font size
+                }}>
+                    <p style={{ margin: 0 }}>{`Ora: ${formattedDate}`}</p>
+                    <p style={{ margin: 0 }}>{`Temperatura: ${payload[0].value}°C`}</p>
+                </div>
+            );
+        }
+
+        return null;
+    };
+    // Ottieni l'ora attuale in formato ISO
+    const currentDate = new Date();
+    const currentHourISO = `${currentDate.toISOString().slice(0, 10)}T${currentDate.getHours().toString().padStart(2, '0')}:00`;
+
+
+
     const renderLineChart = (
-        <ResponsiveContainer width="100%" height={300}>
+        <ResponsiveContainer width="100%" height={200} >
 
-            <AreaChart data={hourlyTemp} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+            <AreaChart data={tempTrend} margin={{ top: 5, right: 20, bottom: 5, left: -40 }}>
                 <Area type="monotone" dataKey="temperature" stroke="#8884d8" />
                 <CartesianGrid stroke="#ccc" strokeDasharray="2 2" />
-                <XAxis dataKey="time" />
-                <YAxis />
-                <Tooltip />
+                <XAxis
+                    dataKey="time"
+                    tickFormatter={(time) => format(new Date(time), 'HH:mm')}
+                    tick={{ fontSize: 6 }}
+                />
+                <YAxis
+                    tick={{ fontSize: 6 }}
+                    domain={[
+                        tempTrend && tempTrend.length > 0 ? Math.min(...tempTrend.map(t => t.temperature)) - 2 : 0,
+                        tempTrend && tempTrend.length > 0 ? Math.max(...tempTrend.map(t => t.temperature)) + 2 : 5,
+                    ]}
+                />
+                <Tooltip
+                    content={<CustomTooltip />}
+                />
+                <ReferenceLine
+                    x={currentHourISO}
+                    stroke="red"
+                    strokeDasharray="3 3"
+                    label={{ value: "Adesso", fill: "red", fontSize: 8, angle: 90, offset: 10, position: 'right' }} // Modifica fontSize per rimpicciolire il testo 
+                />
             </AreaChart>
         </ResponsiveContainer>
     );
+
+
+
     return (
         <>
-            {renderLineChart}
+            {tempTrend?.length > 0 && <>
+                {renderLineChart}
+                <div
+                    className='text-center'>
+                    <strong>Temperatura nelle prossime 24 ore</strong>
+                </div>
+            </>
+            }
         </>
     )
 }
